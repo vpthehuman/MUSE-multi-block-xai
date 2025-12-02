aecfc2e83b2212ff967db742d3bbc3c4e0245e88
Vishnupriya Kannan <155794927+vpthehuman@users.noreply.github.com>
2025-12-03 00:38:25 +0530

    Update __init__.py

--- src/    muse_xai/__init__.py
+++ src/    muse_xai/__init__.py
@@ -1,4 +1,5 @@
-# update version in src/muse_xai/__init__.py
+__all__ = ["MUSE"]
+__version__ = "0.1.0"
 git add src/muse_xai/__init__.py
 git commit -m "Bump package version to 0.1.0"
 git tag -a v0.1.0 -m "v0.1.0 release"
